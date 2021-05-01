7787c1bb5c9443b7180d3983fafac574bfe85982
Ashreya Jayaram <ajayaram@uni-mainz.de>
2021-05-01 12:32:04 +0200

    added RNG identifier for anisotropic brownian integrator

--- hoomd/RNGIdentifiers.h
+++ hoomd/RNGIdentifiers.h
@@ -38,6 +38,7 @@ struct RNGIdentifier
     static const uint32_t EvaluatorPairDPDThermo = 0x4a84f5d0;
     static const uint32_t IntegrationMethodTwoStep = 0x11df5642;
     static const uint32_t TwoStepBD = 0x431287ff;
+    static const uint32_t TwoStepBDAnisoGamma = 0x5a2219bf;
     static const uint32_t TwoStepLangevin = 0x89abcdef;
     static const uint32_t TwoStepLangevinAngular = 0x19fe31ab;
     static const uint32_t TwoStepNPTMTK = 0x9db2f0ab;
